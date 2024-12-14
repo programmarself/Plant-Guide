646fffa57d39a435fdfe9d89e82fea3d107f7035
IRFAN ULLAH KHAN <134633289+programmarself@users.noreply.github.com>
2024-12-14 21:22:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def plants():
     return render_template('plants.html', plants=filtered_data.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
