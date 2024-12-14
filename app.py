91a8901e7fc0862d696b3f2647f55789f3421790
IRFAN ULLAH KHAN <134633289+programmarself@users.noreply.github.com>
2024-12-14 21:57:06 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load data
+data = pd.concat([
+    pd.read_csv('summer.csv',encoding='latin1').assign(Category='Summer'),
+    pd.read_csv('winter.csv',encoding='latin1').assign(Category='Winter'),
+    pd.read_csv('spring.csv',encoding='latin1').assign(Category='Spring')
+])
+
+@app.route('/')
+def home():
+    categories = data['Category'].unique()
+    return render_template('index.html', categories=categories)
+
+@app.route('/plants')
+def plants():
+    category = request.args.get('category')
+    query = request.args.get('query', '').lower()
+    filtered_data = data
+
+    if category:
+        filtered_data = filtered_data[filtered_data['Category'] == category]
+    if query:
+        filtered_data = filtered_data[
+            filtered_data['Name'].str.lower().str.contains(query)
+        ]
+    return render_template('plants.html', plants=filtered_data.to_dict(orient='records'))
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0')
